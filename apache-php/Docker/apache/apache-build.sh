ba4140a00974d4c66bde5d1e3b5cb5a37cb646ba
畠山忠修 <tadanobu@hatakeyamatadashiosamunoMacBook-Pro.local>
2020-03-07 22:17:03 +0900

    apache-php追加

--- apache-php/Docker/apache/apache-build.sh
+++ apache-php/Docker/apache/apache-build.sh
@@ -5,6 +5,8 @@ cp /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf /opt/rh/httpd24/root/etc/httpd
 sed -i "s/    DirectoryIndex index.html/    DirectoryIndex index.php/" /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
 sed -i "s/#ServerName www.example.com:80/ServerName localhost:80/" /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
 sed -i "s/Listen 80/Listen 80\nListen 10080/" /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
+sed -i "s/User apache/User www-data/" /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
+sed -i "s/Group apache/Group www-data/" /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
 
 echo "# クリックジャッキング対策" >> /opt/rh/httpd24/root/etc/httpd/conf/httpd.conf
 echo "Header always append X-Frame-Options SAMEORIGIN" \
